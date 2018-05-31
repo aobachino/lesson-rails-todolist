3625d371942e95d7efba5cfa3758ddf801a82cc1
nachtmusik <to.akiyama.dev@gmail.com>
2018-05-31 22:23:00 +0900

    サインアップ時にuser.nameを入力出来るように変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180531113931) do
+ActiveRecord::Schema.define(version: 20180531130839) do
 
   create_table "statuses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
