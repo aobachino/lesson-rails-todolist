4c4928506f8db3cd61454d5faba22f81a8b5683b
nachtmusik <to.akiyama.dev@gmail.com>
2018-06-08 22:08:47 +0900

    datatablesの読み込み挙動安定化（jsの記述場所変更)

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,31 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(document).ready(function() {
-  $('#all-task-table').DataTable({
-    "bJQueryUI": true,
-    // "sPaginationType": "full_numbers"
-    buttons: [
-      {
-          extend:    'copyHtml5',
-          text:      '<i class="fa fa-files-o"></i>',
-          titleAttr: 'Copy'
-      },
-      {
-          extend:    'excelHtml5',
-          text:      '<i class="fa fa-file-excel-o"></i>',
-          titleAttr: 'Excel'
-      },
-      {
-          extend:    'csvHtml5',
-          text:      '<i class="fa fa-file-text-o"></i>',
-          titleAttr: 'CSV'
-      },
-      {
-          extend:    'pdfHtml5',
-          text:      '<i class="fa fa-file-pdf-o"></i>',
-          titleAttr: 'PDF'
-      }
-    ]
-  });
-
-} );
+// $(document).ready(function() {
+//   $('#all-task-table').DataTable({
+//     "bJQueryUI": true,
+//     // "sPaginationType": "full_numbers"
+//   });
+// } );
