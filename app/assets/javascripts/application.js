67b95132ab8df1fd6af68c2580bf391125dc8d81
nachtmusik <to.akiyama.dev@gmail.com>
2018-06-08 17:35:27 +0900

    dataTables追加->tasks/indexに適用。（アイコン表示されないバグあり)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,9 @@
 //= require turbolinks
 //= require jquery
 //= require jquery_ujs
+//= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+//= require dataTables/jquery.dataTables
 //= require popper
 //= require bootstrap
 //= require_tree .
